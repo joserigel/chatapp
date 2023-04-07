f62487060d53fa887098e105f5afe73afe8d7a97
joserigel <joserigel18@gmail.com>
2023-04-07 19:15:14 +0200

    Cleaned up code for login

--- frontend/src/app/authorization.service.ts
+++ frontend/src/app/authorization.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable, firstValueFrom, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 
@@ -13,12 +14,36 @@ interface Token {
   providedIn: 'root'
 })
 export class AuthorizationService {
-  private url = "/api/login"
+  private username: string = '';
+
+  private url = "/api/login";
   private httpOptions = {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   }
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService) { }
 
+  async login(username: string, password: string): Promise<boolean> {
+    const observable = this.http.post<Token>(this.url,
+      JSON.stringify({
+        username, password
+      }),
+      this.httpOptions
+    ).pipe(catchError((e) => {return of(e)}));
+
+    const {token} = await firstValueFrom(observable);
+    if (token) {
+      this.username = username;
+      this.cookieService.set('token', token);
+      return true;
+    } else {
+      this.username = '';
+      return false;
+    }
+  }
+
+  /*
   login(username: string, password: string): Observable<Token> {
     return this.http.post<Token>(this.url,
       JSON.stringify({
@@ -27,4 +52,5 @@ export class AuthorizationService {
       this.httpOptions
     ).pipe(catchError((e) => {return of(e)}));
   }
+  */
 }
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { AuthorizationService } from '../authorization.service';
 import { Router } from '@angular/router';
 
-import { CookieService } from 'ngx-cookie-service';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -16,27 +14,20 @@ export class LoginComponent {
 
   constructor (
     private authorization: AuthorizationService,
-    private router: Router,
-    private cookieService: CookieService
+    private router: Router
   ) {}
 
   login(): void {
-    const credentials = this.authorization.login(this.username, this.password);
-    
-    credentials.subscribe((obj) => {
-      const { token } = obj;
-
-      if (token) {
+    const auth = this.authorization.login(this.username, this.password);
+    auth.then((res: boolean) => {
+      if (res) {
         this.error = false;
-
-        this.cookieService.set('token', token);
-
         this.router.navigate(['/chat']);
       } else {
         this.error = true;
         this.username = '';
         this.password = '';
       }
-    });
+    })
   }
 }
